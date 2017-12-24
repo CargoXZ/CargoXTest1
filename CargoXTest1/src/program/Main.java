bd1df6d6194e1e43ec7c737917b43dea695174ee
CargoXZ <cramosva@yahoo.com>
2017-12-23 21:00:23 -0500

    Created Main; primarily test in adding the Gridworld API JAR file
    Created Main; primarily test in adding the Gridworld API JAR file

---
+++ CargoXTest1/src/program/Main.java
@@ -0,0 +1,15 @@
+/* The class the contains the main method  for the program.
+ * 
+ */
+
+
+
+package program;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		//TODO make stuff work
+	}
+}
